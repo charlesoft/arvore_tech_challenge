908b370db05445a5f6f654086f8b6f406bb25c02
Charles Furtado <charlees1509@hotmail.com>
2023-11-08 15:27:20 -0300

    Add seeds data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,46 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ArvoreTechChallenge.Repo.insert!(%ArvoreTechChallenge.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias ArvoreTechChallenge.{Entity, Repo}
+
+school_one =
+  %Entity{
+    name: "School Example 1",
+    entity_type: :school,
+    inep: "123213",
+    parent_id: nil
+  }
+  |> Repo.insert!()
+
+network =
+  %Entity{
+    name: "Network Example",
+    entity_type: :network,
+    inep: nil,
+    parent_id: nil
+  }
+  |> Repo.insert!()
+
+school_two =
+  %Entity{
+    name: "School Example 2",
+    entity_type: :school,
+    inep: "123456",
+    parent_id: network.id
+  }
+  |> Repo.insert!()
+
+_class_one =
+  %Entity{
+    name: "Class Example 1",
+    entity_type: :class,
+    inep: nil,
+    parent_id: school_one.id
+  }
+  |> Repo.insert!()
+
+_class_two =
+  %Entity{
+    name: "Class Example 2",
+    entity_type: :class,
+    inep: nil,
+    parent_id: school_two.id
+  }
+  |> Repo.insert!()
