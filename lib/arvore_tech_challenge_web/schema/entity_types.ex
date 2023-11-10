43a674c84cd9f5c901547ec4b886eb5968ca555c
Charles Furtado <charlees1509@hotmail.com>
2023-11-10 12:45:37 -0300

    Add EntityTypes.

---
+++ lib/arvore_tech_challenge_web/schema/entity_types.ex
@@ -0,0 +1,12 @@
+defmodule ArvoreTechChallengeWeb.Schema.EntityTypes do
+  use Absinthe.Schema.Notation
+
+  object :entity do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :entity_type, non_null(:string)
+    field :parent_id, :id
+    field :inep, :string
+    field :entities, list_of(:id)
+  end
+end
