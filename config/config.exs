378c5f21a55b1ec1e8d8b29060b0b4158493eea9
Charles Furtado <charlees1509@hotmail.com>
2023-11-11 23:10:57 -0300

    Add Guardian module and set up default configs for guardian.

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configures Guardian Authentication
+config :arvore_tech_challenge, ArvoreTechChallenge.Accounts.Guardian,
+  issuer: "cumbuca_code_challenge",
+  secret_key: "HcOlVGE0wFcQf4cPNg1SH4JBDI+BlxmZf6aPZXFmvb8QC9a5oLgI/RIxTGNZA+8q"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
