29af46408f58eeeb2dde725ad617d84e327cee0b
Charles Furtado <charlees1509@hotmail.com>
2023-11-11 23:05:00 -0300

    Add migration to create users table.

---
+++ priv/repo/migrations/20231111154546_create_users_table.exs
@@ -0,0 +1,12 @@
+defmodule ArvoreTechChallenge.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string, null: false
+      add :password, :string, null: false
+
+      timestamps()
+    end
+  end
+end
