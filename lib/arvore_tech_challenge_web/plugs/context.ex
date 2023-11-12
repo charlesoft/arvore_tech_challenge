8cf2ab4aff88b6f52fe14472c27e136cf43b34ee
Charles Furtado <charlees1509@hotmail.com>
2023-11-11 23:11:48 -0300

    Add own Absinthe Context plug.

---
+++ lib/arvore_tech_challenge_web/plugs/context.ex
@@ -0,0 +1,29 @@
+defmodule ArvoreTechChallengeWeb.Context do
+  @moduledoc """
+  Absinthe Context to authenticate user
+  """
+  @behaviour Plug
+
+  import Plug.Conn
+
+  alias ArvoreTechChallenge.Accounts.Guardian
+
+  def init(opts), do: opts
+
+  def call(conn, _) do
+    case build_context(conn) do
+      {:ok, context} ->
+        put_private(conn, :absinthe, %{context: %{current_user: context}})
+
+      _ ->
+        conn
+    end
+  end
+
+  defp build_context(conn) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+         {:ok, current_user, _claims} <- Guardian.resource_from_token(token) do
+      {:ok, current_user}
+    end
+  end
+end
